ac94f6b3dfe43f345061623e57df5cd22c4df620
root <root@ip-172-31-28-246.us-east-2.compute.internal>
2021-12-03 02:19:01 +0000

    test

--- html/individual_sample_reviewtest.php
+++ html/individual_sample_reviewtest.php
@@ -5,11 +5,17 @@
     $placeidx = intval($_SESSION["placeid"]);
     $place = $_SESSION["search_results"][$placeidx]["name"]; 
     $name = $_POST["name"];
- 
+    if (empty($name)) {
+        $name = NULL;
+    } 
     $rating = $_POST["rating"];
-        
+    if (empty($rating)) {
+        $rating = NULL;
+    }    
     $comment = $_POST["comment"];
-      
+    if (empty($comment)) {
+        $comment = NULL;
+    }   
 
     $sql = "INSERT INTO reviews (place, name, rating, comment) VALUES (:place,:name,:rating,:comment)";
     $stmt = $conn->prepare($sql);
