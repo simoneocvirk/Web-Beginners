c9acf48d5296e54f8dd90b7bdd819c0c55a343bf
root <root@ip-172-31-28-246.us-east-2.compute.internal>
2021-12-03 06:51:28 +0000

    test

--- html/search.php
+++ html/search.php
@@ -83,7 +83,7 @@ session_start();
     	<h2 class="center">Search it up by the names!</h2>
     	
     	<br>
-        <form class="search" method="post" action="search_word.php"> 
+        <form class="search" method="post" action="search_wordtest.php"> 
     		<input type="search" id="search" name="search" placeholder="Ex: centro" class="searchTerm"> <br>
     		<!--link to another page by sumbit button-->
             <input type="submit" value="Submit" class="searchButton">
@@ -94,7 +94,7 @@ session_start();
   	<!-- Start of the second search panel -->
 		<div class="col-6 center" style="padding: 30px;" >
             <h2>Or...search it up by ratings!</h2>
-                <form method="post" action="search_rating.php">
+                <form method="post" action="search_ratingtest.php">
 				    <select name="rating">
 						<option value="*" disabled selected>Please select...</option>
 						<option value="5">5 star rating</option>
